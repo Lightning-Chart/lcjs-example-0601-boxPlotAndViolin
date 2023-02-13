89da996efc7dc14449b46f68ea856408349d849f
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:48:05 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript Box And Violin Chart
 
-![JavaScript Box And Violin Chart](boxPlotAndViolin.png)
+![JavaScript Box And Violin Chart](boxPlotAndViolin-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,13 +8,13 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
@@ -43,34 +43,34 @@ This example shows usage of BoxSeries in extravagant manner.
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[Color palettes]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#colorpalettes
-[Solid FillStyle]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/solidfill.html
-[Solid LineStyle]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/solidline.html
-[XY cartesian chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chartxy.html
-[cursor modes]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/enums/autocursormodes.html
-[cursor]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/interfaces/autocursorxy.html
-[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/axis.html
-[Scroll strategies]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#axisscrollstrategies
-[Box series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/boxseries.html
-[Box and whiskers Figure]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/boxandwhiskers.html
-[Horizontal Legend box]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#legendboxbuilders.horizontallegendbox
-[UI dragging modes]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/enums/uidraggingmodes.html
-[UI position origins]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#uiorigins
+[Color palettes]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/ColorPalettes.html
+[Solid FillStyle]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/SolidFill.html
+[Solid LineStyle]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/SolidLine.html
+[XY cartesian chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ChartXY.html
+[cursor modes]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/enums/AutoCursorModes.html
+[cursor]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/interfaces/AutoCursorXY.html
+[Axis]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Axis.html
+[Scroll strategies]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/AxisScrollStrategies.html
+[Box series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/BoxSeries.html
+[Box and whiskers Figure]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/BoxAndWhiskers.html
+[Horizontal Legend box]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/LegendBoxBuilders.html
+[UI dragging modes]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/enums/UIDraggingModes.html
+[UI position origins]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/UIOrigins.html
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
 // Import LightningChartJS
 const lcjs = require('@arction/lcjs')
 
+// Import xydata
+const xydata = require('@arction/xydata')
+
 // Extract required parts from LightningChartJS.
 const {
     emptyLine,
@@ -13,26 +16,18 @@ const {
     AxisScrollStrategies,
     AxisTickStrategies,
     AutoCursorModes,
-    Themes
+    Themes,
 } = lcjs
 
 // Import data-generator from 'xydata'-library.
-const {
-    createProgressiveFunctionGenerator
-} = require('@arction/xydata')
+const { createProgressiveFunctionGenerator } = xydata
 
 // Utilities for graphing distribution functions.
 //#region
-const cumulativeDistribution = (
-    probabilityDistributionFunction,
-    rangeMin,
-    rangeMax,
-    step
-) => {
+const cumulativeDistribution = (probabilityDistributionFunction, rangeMin, rangeMax, step) => {
     // Simulate values of respective probability density function.
     const probabilityValues = []
-    for (let x = rangeMin; x <= rangeMax; x += step)
-        probabilityValues.push(probabilityDistributionFunction(x))
+    for (let x = rangeMin; x <= rangeMax; x += step) probabilityValues.push(probabilityDistributionFunction(x))
     // Normalize probability values and push them to a cached array.
     const probabilitySum = probabilityValues.reduce((prev, cur) => prev + cur, 0)
     const values = []
@@ -44,47 +39,37 @@ const cumulativeDistribution = (
     }
     // Return function that returns the closest value (by x) from the cached 'values' array.
     return (x) => {
-        const xAsIndex = (x - rangeMin) / (rangeMax - rangeMin) * ((rangeMax - rangeMin) / step)
+        const xAsIndex = ((x - rangeMin) / (rangeMax - rangeMin)) * ((rangeMax - rangeMin) / step)
         // Pick closest index (left/right) that exists
         const closestIndex = Math.min(Math.max(Math.round(xAsIndex), 0), values.length - 1)
         return values[closestIndex]
     }
 }
-const findQuartileX = (
-    yToLookFor,
-    cumulativeDistributionFunction,
-    rangeMin,
-    rangeMax,
-    step
-) => {
+const findQuartileX = (yToLookFor, cumulativeDistributionFunction, rangeMin, rangeMax, step) => {
     // Iterate over possible 'X' values and pick the one where resulting 'Y' is closest to 'yToLookFor'
     let bestResult
     for (let x = rangeMin; x <= rangeMax; x += step) {
         const y = cumulativeDistributionFunction(x)
         const delta = Math.abs(y - yToLookFor)
-        if (bestResult === undefined || delta < bestResult.delta)
-            bestResult = { x, delta }
-        else if (bestResult !== undefined && delta > bestResult.delta)
-            break
+        if (bestResult === undefined || delta < bestResult.delta) bestResult = { x, delta }
+        else if (bestResult !== undefined && delta > bestResult.delta) break
     }
     return bestResult.x
 }
-const probabilityDistribution = (mean, variance) =>
-    (x) =>
-        (1 / (variance * Math.sqrt(2 * Math.PI))) * Math.pow(Math.E, -Math.pow((x - mean), 2) / (2 * variance * variance))
+const probabilityDistribution = (mean, variance) => (x) =>
+    (1 / (variance * Math.sqrt(2 * Math.PI))) * Math.pow(Math.E, -Math.pow(x - mean, 2) / (2 * variance * variance))
 //#endregion
 
 // Make chart with series graphing standard probability density and cumulative distribution functions.
-const chart = lightningChart().ChartXY({
-    // theme: Themes.darkGold 
-})
+const chart = lightningChart()
+    .ChartXY({
+        // theme: Themes.darkGold
+    })
     .setTitle('Probability distribution + Simulated accumulation and BoxSeries')
     // Set auto-cursor mode to 'onHover'
     .setAutoCursorMode(AutoCursorModes.onHover)
-    .setAutoCursor((cursor) => cursor
-        .setResultTableAutoTextStyle(false)
-        .setTickMarkerXAutoTextStyle(false)
-        .setTickMarkerYAutoTextStyle(false)
+    .setAutoCursor((cursor) =>
+        cursor.setResultTableAutoTextStyle(false).setTickMarkerXAutoTextStyle(false).setTickMarkerYAutoTextStyle(false),
     )
     .setPadding({ right: 20 })
 
@@ -93,8 +78,9 @@ const step = 0.02
 // Setup axes.
 const axisDistribution = chart.getDefaultAxisY()
 const axisNormalized = chart.addAxisY()
-const axisX = chart.getDefaultAxisX()
-    .setInterval(xBounds.min, xBounds.max)
+const axisX = chart
+    .getDefaultAxisX()
+    .setInterval({ start: xBounds.min, end: xBounds.max, stopAxisAfter: false })
     .setScrollStrategy(undefined)
 
 // Set up the Distribution Axis.
@@ -106,57 +92,43 @@ axisDistribution
         // Use Numeric TickStrategy as base.
         AxisTickStrategies.Numeric,
         // Use mutator to modify the TickStrategy.
-        tickStrategy => tickStrategy
-            // Modify Major Tick Style by using a mutator.
-            .setMajorTickStyle(
-                tickStyle => tickStyle
-                    .setGridStrokeStyle(emptyLine)
-            )
-            // Modify Minor Tick Style by using a mutator.
-            .setMinorTickStyle(
-                tickStyle => tickStyle
-                    .setGridStrokeStyle(emptyLine)
-            )
+        (tickStrategy) =>
+            tickStrategy
+                // Modify Major Tick Style by using a mutator.
+                .setMajorTickStyle((tickStyle) => tickStyle.setGridStrokeStyle(emptyLine))
+                // Modify Minor Tick Style by using a mutator.
+                .setMinorTickStyle((tickStyle) => tickStyle.setGridStrokeStyle(emptyLine)),
     )
 
 // Set up the Normalized Axis.
 axisNormalized
     .setTitle('Accumulated distribution (%)')
-    .setInterval(0, 1)
+    .setInterval({ start: 0, end: 1, stopAxisAfter: false })
     .setScrollStrategy(undefined)
     // Modify the TickStrategy to remove gridLines from this Y Axis.
     .setTickStrategy(
         // Use Numeric TickStrategy as base.
         AxisTickStrategies.Numeric,
         // Use mutator to modify the TickStrategy.
-        tickStrategy => tickStrategy
-            // Modify Major Tick Style by using a mutator.
-            .setMajorTickStyle(
-                tickStyle => tickStyle
-                    .setGridStrokeStyle(emptyLine)
-            )
-            // Modify Minor Tick Style by using a mutator.
-            .setMinorTickStyle(
-                tickStyle => tickStyle
-                    .setGridStrokeStyle(emptyLine)
-            )
+        (tickStrategy) =>
+            tickStrategy
+                // Modify Major Tick Style by using a mutator.
+                .setMajorTickStyle((tickStyle) => tickStyle.setGridStrokeStyle(emptyLine))
+                // Modify Minor Tick Style by using a mutator.
+                .setMinorTickStyle((tickStyle) => tickStyle.setGridStrokeStyle(emptyLine)),
     )
 
 // Cumulative distribution.
-const cumulativeDistributionSeries = chart.addAreaSeries({ yAxis: axisNormalized })
-    .setName('Simulated Cumulative Distribution')
+const cumulativeDistributionSeries = chart.addAreaSeries({ yAxis: axisNormalized }).setName('Simulated Cumulative Distribution')
 
 // Probability distribution.
-const probabilityDistributionSeries = chart.addAreaSeries({ yAxis: axisDistribution })
-    .setName('Probability Distribution')
+const probabilityDistributionSeries = chart.addAreaSeries({ yAxis: axisDistribution }).setName('Probability Distribution')
 
 // 'Violin' series.
-const violinSeries = chart.addAreaRangeSeries({ yAxis: axisDistribution })
-    .setName('Violin')
+const violinSeries = chart.addAreaRangeSeries({ yAxis: axisDistribution }).setName('Violin')
 
 // Box series.
-const boxSeries = chart.addBoxSeries({ yAxis: axisDistribution, dimensionStrategy: yDimensionStrategy })
-    .setName('Box')
+const boxSeries = chart.addBoxSeries({ yAxis: axisDistribution, dimensionStrategy: yDimensionStrategy }).setName('Box')
 
 // Drawing logic
 //#region
@@ -174,8 +146,7 @@ const graphDistribution = (mean, variance) => {
     const streamInterval = 30
 
     // Reset interval if user isn't up to something.
-    if (!axisDistribution.isStopped())
-        axisDistribution.setInterval(0, 1.0)
+    if (!axisDistribution.getStopped()) axisDistribution.setInterval({ start: 0, end: 1.0, stopAxisAfter: false })
 
     createProgressiveFunctionGenerator()
         .setSamplingFunction(cumulativeDistributionFunction)
@@ -183,7 +154,7 @@ const graphDistribution = (mean, variance) => {
         .setEnd(xBounds.max)
         .setStep(step)
         .generate()
-        .setStreamBatchSize(1000 * (xBounds.max - xBounds.min) / (step * streamInterval * streamDuration))
+        .setStreamBatchSize((1000 * (xBounds.max - xBounds.min)) / (step * streamInterval * streamDuration))
         .setStreamInterval(streamInterval)
         .toStream()
         .forEach((point) => cumulativeDistributionSeries.add(point))
@@ -193,7 +164,7 @@ const graphDistribution = (mean, variance) => {
         .setEnd(xBounds.max)
         .setStep(step)
         .generate()
-        .setStreamBatchSize(1000 * (xBounds.max - xBounds.min) / (step * streamInterval * streamDuration))
+        .setStreamBatchSize((1000 * (xBounds.max - xBounds.min)) / (step * streamInterval * streamDuration))
         .setStreamInterval(streamInterval)
         .toStream()
         .forEach((point) => {
@@ -203,7 +174,7 @@ const graphDistribution = (mean, variance) => {
                 violinSeries.add({
                     position: point.x,
                     high: 1.0 + point.y / 2,
-                    low: 1.0 - point.y / 2
+                    low: 1.0 - point.y / 2,
                 })
         })
 
@@ -211,19 +182,19 @@ const graphDistribution = (mean, variance) => {
     setTimeout(() => {
         // Find quartile values using cumulative distribution function.
         const q1 = findQuartileX(0.25, cumulativeDistributionFunction, xBounds.min, xBounds.max, step)
-        const q2 = findQuartileX(0.50, cumulativeDistributionFunction, xBounds.min, xBounds.max, step)
+        const q2 = findQuartileX(0.5, cumulativeDistributionFunction, xBounds.min, xBounds.max, step)
         const q3 = findQuartileX(0.75, cumulativeDistributionFunction, xBounds.min, xBounds.max, step)
         const iqr = q3 - q1
         const boxSeriesDimensions = {
             // Purely visual 'Y' -dimensions
-            start: 0.90,
-            end: 1.10,
+            start: 0.9,
+            end: 1.1,
             // Actual data -dimensions along 'X' -axis
             lowerExtreme: q1 - 1.5 * iqr,
             lowerQuartile: q1,
             median: q2,
             upperQuartile: q3,
-            upperExtreme: q3 + 1.5 * iqr
+            upperExtreme: q3 + 1.5 * iqr,
         }
         boxSeries.add(boxSeriesDimensions)
     }, streamDuration)
@@ -233,16 +204,17 @@ const graphDistribution = (mean, variance) => {
 graphDistribution(0, 1)
 
 // Add LegendBox as part of chart.
-const legend = chart.addLegendBox(LegendBoxBuilders.HorizontalLegendBox )
+const legend = chart
+    .addLegendBox(LegendBoxBuilders.HorizontalLegendBox)
     // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
     .setAutoDispose({
         type: 'max-width',
-        maxWidth: 0.80,
+        maxWidth: 0.8,
     })
 legend.add(chart)
 
 cumulativeDistributionSeries.setCursorResultTableFormatter((tableBuilder, rangeSeries, position, high, low) => {
-    const x = (position.toFixed(2) == '-0.00') ? '0.00' : position.toFixed(2);
+    const x = position.toFixed(2) == '-0.00' ? '0.00' : position.toFixed(2)
     return tableBuilder
         .addRow('Simulated Cumulative Distribution')
         .addRow('Position ' + x)
@@ -250,7 +222,7 @@ cumulativeDistributionSeries.setCursorResultTableFormatter((tableBuilder, rangeS
         .addRow('Base ' + low.toFixed(2))
 })
 probabilityDistributionSeries.setCursorResultTableFormatter((tableBuilder, rangeSeries, position, high, low) => {
-    const x = (position.toFixed(2) == '-0.00') ? '0.00' : position.toFixed(2);
+    const x = position.toFixed(2) == '-0.00' ? '0.00' : position.toFixed(2)
     return tableBuilder
         .addRow('Probability Distribution')
         .addRow('Position ' + x)
@@ -258,7 +230,7 @@ probabilityDistributionSeries.setCursorResultTableFormatter((tableBuilder, range
         .addRow('Base ' + low.toFixed(2))
 })
 violinSeries.setCursorResultTableFormatter((tableBuilder, rangeSeries, position, high, low) => {
-    const x = (position.toFixed(2) == '-0.00') ? '0.00' : position.toFixed(2);
+    const x = position.toFixed(2) == '-0.00' ? '0.00' : position.toFixed(2)
     return tableBuilder
         .addRow('Violin')
         .addRow('Position ' + x)
